c265f90c01781987c64a6dcf5a64133c139f8f66
Sevann Radhak <Sradhak@andromaco.com.ar>
2019-10-25 15:32:34 -0300

    first api conection

--- src/app/marca/marca.component.ts
+++ src/app/marca/marca.component.ts
@@ -10,14 +10,83 @@ import {MarcasService} from './../services/marcas.service'
 })
 export class MarcaComponent implements OnInit {
 
-  // Services and other things
-  marcas: any;
+  // Constructor
   constructor(private marcasService: MarcasService){
-    // console.log(JSON.stringify(marcasService.marcas.rows));
-    this.marcas = marcasService.marcas.rows;
+    this.getMarcas();
   }
+
+  // Object definitions
+  newMarca: any = {
+    Codigo: '',
+    Nombre: ''
+  };
+  marcas: any;
+
 
   ngOnInit() {
   }
+
+  // Crud methods
+  // Add a new competidor
+  getMarcas():void{
+    this.marcasService.getMarcas().subscribe(result => {
+      this.marcas = result;
+    },
+    error =>{
+      console.log(JSON.stringify(error));
+      
+    });
+    // this.competidores.push(this.model);
+    // this.msg = 'Registro almacenado exitosamente';
+  }
+
+  postMarca():void {
+    console.log(this.newMarca);
+    this.marcasService.postMarca(this.newMarca).subscribe(result => {
+      this.getMarcas();
+    },
+    error =>{
+      console.log(JSON.stringify(error));
+      
+    });
+  }
+  
+  // Delete a competidor
+  deleteMarca(id: string):void{
+    this.marcasService.deleteMarca(id).subscribe(result => {
+      this.getMarcas();
+    },
+    error => {
+      console.log(error);
+    });
+    
+    // var answer = confirm('¿Estás seguro de eliminar el registro?');
+    
+    // if(answer){
+    //   this.competidores.splice(i, 1);
+    //   this.msg = 'Registro eliminado exitosamente';
+    // }
+  }
+  
+  // Edit a competidor
+  // myValue;
+  // editCompetidor(i):void{
+  //   this.hideUpdate = false;
+
+  //   this.modelToUpdate.name = this.competidores[i].name;
+  //   this.modelToUpdate.position = this.competidores[i].position;
+  //   this.modelToUpdate.email = this.competidores[i].email;
+
+  //   this.myValue = i;
+  // }
+  
+  // // Update a competidor
+  // updateCompetidor():void{
+  //   let i = this.myValue;
+
+  //   this.competidores[i] = this.modelToUpdate;
+  //   this.modelToUpdate = {};
+  //   this.msg = 'Registro actualizado exitosamente';
+  // }
 
 }
